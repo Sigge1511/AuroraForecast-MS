ee9c4e15fa233080340b733326110f45398fc1c5
Maja Sigfeldt <msigfeldt@gmail.com>
2026-02-01 23:46:05 +0100

    fixed search box style

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -5,6 +5,14 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoUnderline", (handler, view) =>
+        {
+            #if WINDOWS
+            // Tar bort den inbyggda linjen och fokus-markeringen på Windows
+            handler.PlatformView.BorderThickness = new Microsoft.UI.Xaml.Thickness(0);
+            handler.PlatformView.Resources["TextControlBorderThemeThicknessFocused"] = new Microsoft.UI.Xaml.Thickness(0);
+            #endif
+        });
     }
 
     private async void OnSearchClicked(object? sender, EventArgs e)
